8fc35b8d8be7934d533982208fee29a0ae37f263
Maxence Ramael <maxence.ramael@gmail.com>
2022-01-11 15:37:02 +0100

    fin formulaire contact desktop

--- src/Components/Contact/FormContact.js
+++ src/Components/Contact/FormContact.js
@@ -1,32 +1,67 @@
 import React, { useState } from "react";
 import "./FormContact.css";
 
-
- function FormContact() {
+function FormContact() {
   const [name, setName] = useState("");
   const [company, setCompany] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
 
+  const isEmail = () => {
+    let mail = document.getElementById("not-mail");
+    let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+    if (email.match(regex)) {
+      mail.style.display = "none";
+      return true;
+    } else {
+      mail.style.display = "block";
+      mail.style.animation = "dongle 1s";
+      setTimeout(() => {
+        mail.style.animation = "none";
+      }, 1000);
+      return false;
+    }
+  };
+
+  const failMessage = () => {
+    let formMess = document.querySelector(".form-message");
+    formMess.innerHTML = "Merci de remplir correctement les champs requis *";
+    formMess.style.oppacity = "1";
+    formMess.style.background = "rgb(253,87,87)";
+  };
+
+  const oKMessage = () => {
+    let okMess = document.querySelector(".form-message");
+    okMess.innerHTML = "Message envoyé ! Merci";
+    okMess.style.oppacity = "1";
+    okMess.style.background = "lightgreen";
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    sendFeedback("template_i0nrewf", {
-      name,
-      company,
-      phone,
-      email,
-      message,
-    });
+    if (name && isEmail() && message) {
+      sendFeedback("template_i0nrewf", {
+        name,
+        company,
+        phone,
+        email,
+        message,
+      });
+      oKMessage();
+    } else {
+      failMessage();
+    }
   };
 
   const sendFeedback = (templateId, variables) => {
-    console.log(templateId)
+    console.log(templateId);
     console.log(variables);
 
     window.emailjs
-      .send("service_r3xv167", templateId, variables,)
+      .send("service_r3xv167", templateId, variables)
       .then((res) => {
         console.log("success !");
         setName("");
@@ -56,6 +91,7 @@ import "./FormContact.css";
           required
         />
         <div className="email-content">
+          <label id="not-mail">Email non valide</label>
           <input
             type="email"
             name="email"
